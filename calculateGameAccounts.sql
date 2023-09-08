011a2c9649bfec09dd1d46d71bb6803bc3a8ed22
MirageM <imiragesz@gmail.com>
2023-09-08 00:12:46 -0400

    SELECT c.name as customers

---
+++ calculateGameAccounts.sql
@@ -0,0 +1,178 @@
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+FROM accounts a
+JOIN accounts_items ai ON a.id = ai.account_id
+JOIN items i ON ai.item_id = i.id
+GROUP BY a.id, a.username, a.email
+HAVING SUM(i.weight) > 20
+ORDER BY total_weight DESC, username ASC;
+
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+FROM accounts a
+JOIN accounts_items ai ON a.id = ai.account_id
+JOIN items i ON ai.item_id = i.id
+GROUP BY a.id, a.username, a.email
+HAVING SUM(i.weight) > 20
+ORDER BY total_weight DESC, username ASC;
+
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+FROM accounts a
+JOIN accounts_items ai ON a.id = ai.account_id
+JOIN items i ON ai.item_id = i.id 
+GROUP BY a.id, a.username, a.email
+HAVING SUM(i.weight) > 20
+ORDER BY total_weight DESC, username ASC;
+
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+FROM accounts a
+JOIN accounts_items ai ON a.id = ai.account_id
+JOIN items i ON ai.item_id = i.id
+GROUP BY a.id, a.username, a.email
+HAVING SUM(i.weight) > 20
+ORDER BY total_weight DESC, username ASC;
+
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+FROM accounts a
+JOIN accounts_items ai ON a.id = ai.account_id
+JOIN items i ON ai.item_id = i.id
+GROUP BY a.id, a.username, a.email
+HAVING SUM(i.weight) > 20
+ORDER BY total_weight DESC, username ASC;
+
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+FROM accounts a
+JOIN accounts_items ai ON a.id = ai.account_id
+JOIN items i ON ai.item_id = i.id
+GROUP BY a.id, a.username, a.email
+HAVING SUM(i.weight) > 20
+ORDER BY total_weight DESC, username ASC;
+
+
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+FROM accounts a
+JOIN accounts_items ai ON a.id = ai.account_id
+JOIN items i ON ai.item_id = i.id
+GROUP BY a.id, a.username, a.email
+HAVING SUM(i.weight) > 20
+ORDER BY total_weight DESC, username ASC;
+
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+FROM accounts a
+JOIN accounts_items ai ON a.id = ai.account_id
+JOIN items i ON ai.item_id = i.id
+GROUP BY a.id, a.username, a.email
+HAVING SUM(i.weight) > 20
+ORDER BY total_weight DESC, username ASC;
+
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+FROM accounts a
+JOIN accounts_items ai ON a.id = ai.account_id
+JOIN items i ON ai.item_id = i.id
+GROUP BY a.id, a.username, a.email
+HAVING SUM(i.weight) > 20
+ORDER BY total_weight DESC, username ASC;
+
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+FROM accounts a
+JOIN accounts_items ai ON a.id = ai.account_id
+JOIN items i ON ai.item_id = i.id
+GROUP BY a.id, a.username, a.email
+HAVING SUM(i.weight) > 20
+ORDER BY total_weight DESC, username ASC;
+
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+FROM accounts a
+JOIN accounts_items ai ON a.id = ai.account_id
+JOIN items i ON ai.item_id = i.id
+GROUP BY a.id, a.username, a.email
+HAVING SUM(i.weight) > 20
+ORDER BY total_weight DESC, username ASC;
+
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+FROM accounts a
+JOIN accounts_items ai ON a.id = ai.account_id
+JOIN items i ON ai.item_id = i.id
+GROUP BY a.id, a.username, a.email
+HAVING SUM(i.weight) > 20
+ORDER BY total_weight DESC, username ASC;
+
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+FROM accounts a
+JOIN accounts_items ai ON a.id = ai.account_id
+JOIN items i ON ai.item_id = i.id
+GROUP BY a.id, a.username, a.email 
+HAVING SUM(i.weight) > 20
+ORDER BY total_weight DESC, username ASC;
+
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+FROM accounts a
+JOIN accounts_items ai ON a.id = ai.account_id
+JOIN items i ON ai.item_id = i.id
+GROUP BY a.id, a.username, a.email
+HAVING SUM(i.weight) > 20
+ORDER BY total_weight DESC, username ASC;
+
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+FROM accounts a
+JOIN accounts_items ai ON a.id = ai.account_id
+JOIN items i ON ai.item_id = i.id
+GROUP BY a.id, a.username, a.email
+HAVING SUM(i.weight) > 20
+ORDER BY total_weight DESC, username ASC;
+
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+FROM accounts a
+JOIN accounts_items ai ON a.id = ai.account_id
+JOIN items i ON ai.item_id = i.id
+GROUP BY a.id, a.username, a.email
+HAVING SUM(i.weight) > 20
+ORDER BY total_weight DESC, username ASC;
+
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+FROM accounts a
+JOIN accounts_items ai ON a.id = ai.account_id
+JOIN items i ON ai.item_id = i.id
+GROUP BY a.id, a.username, a.email
+HAVING SUM(i.weight) > 20
+ORDER BY total_weight DESC, username ASC;
+
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+FROM accounts a
+JOIN accounts_items ai ON a.id = ai.account_id
+JOIN items i ON ai.item_id = i.id
+GROUP BY a.id, a.username, a.email
+HAVING SUM(i.weight) > 20
+ORDER BY total_weight DESC, username ASC;
+
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+FROM accounts a
+JOIN accounts_items ai ON a.id = ai.account_id
+JOIN items i ON ai.item_id = i.id
+GROUP BY a.id, a.username, a.email
+HAVING SUM(i.weight) > 20
+ORDER BY total_weight DESC, username ASC;
+
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+FROM accounts a
+JOIN accounts_items ai ON a.id = ai.account_id
+JOIN items i ON ai.item_id = i.id
+GROUP BY a.id, a.username, a.email
+HAVING SUM(i.weight) > 20
+ORDER BY total_weight DESC, username ASC;
+
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+FROM accounts a
+JOIN accounts_items ai ON a.id = ai.account_id
+JOIN items i ON ai.item_id = i.id
+GROUP BY a.id, a.username, a.email
+HAVING SUM(i.weight) > 20
+COUNT BY total_weight DESC, username ASC;
+
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
+SELECT a.username, a.email, count(i.id) AS items, SUM(i.weight) AS total_weight
